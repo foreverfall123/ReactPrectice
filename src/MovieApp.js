77239a213436827b3702d15e681fa2114ce63e37
foreverfall123 <foreverfall@naver.com>
2022-09-05 22:37:56 +0900

    movie app create

---
+++ src/MovieApp.js
@@ -0,0 +1,26 @@
+import {
+    BrowserRouter as Router,
+    Routes,
+    Route,
+} from "react-router-dom";
+import Detail from "./routes/Detail";
+import Home from "./routes/Home";
+
+function MovieApp() {
+    return (
+        <Router>
+            <Routes>
+                <Route path="/movie/:id" element={
+                    <Detail />
+                }>
+                </Route>
+                <Route path="/" element={
+                    <Home />
+                }>
+                </Route>
+            </Routes>
+        </Router>
+    );
+  }
+  
+  export default MovieApp;
